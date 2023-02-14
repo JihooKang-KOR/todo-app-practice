e35d4f05d39ee5bd85163b2cf5b0bb0b45aada52
Jihoo Kang <jihoo.kang.kor@gmail.com>
2023-02-14 13:45:04 +0900

    Set a Spring Boot project (#3)

---
+++ src/main/java/com/codestates/TodoAppPracticeForUnit2Application.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppPracticeForUnit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppPracticeForUnit2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/TodoAppPracticeForUnit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppPracticeForUnit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
